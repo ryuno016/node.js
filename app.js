4566c1b1b9148ed0d498bec85094b6160f625308
多多納 龍之介 <ryuno8901398@gmail.com>
2024-08-02 14:14:03 +0900

    更新と削除機能の実装

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true })); // フォームデータの解析
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 const mysql = require("mysql2");
 
@@ -20,26 +22,69 @@ app.get("/", (req, res) => {
   const sql = "SELECT * FROM users";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    res.render("index", { users: result, foundUser: null, message: null }); // 初期表示ではfoundUserとmessageはnull
+    res.render("index", { users: result, foundUser: null, message: null });
   });
 });
 
 app.post("/", (req, res) => {
-  const userId = req.body.userId; // フォームからIDを取得
+  const userId = req.body.userId;
   const sql = "SELECT * FROM users WHERE id = ?";
 
   con.query(sql, [userId], function (err, result) {
     if (err) throw err;
 
     if (result.length > 0) {
-      // ユーザーが見つかった場合
       res.render("index", { users: null, foundUser: result[0], message: null });
     } else {
-      // ユーザーが見つからなかった場合
-      res.render("index", { users: null, foundUser: null, message: "該当するユーザーが見つかりません。" });
+      res.render("index", {
+        users: null,
+        foundUser: null,
+        message: "該当するユーザーが見つかりません。",
+      });
     }
   });
 });
 
+app.get("/edit/:id", (req, res) => {
+  const sql = "SELECT * FROM users WHERE id = ?";
+  con.query(sql, [req.params.id], function (err, result) {
+    if (err) throw err;
+    res.render("edit", { user: result[0] });
+  });
+});
+
+app.post("/update/:id", (req, res) => {
+  const sql = "UPDATE users SET ? WHERE id = ?";
+  con.query(sql, [req.body, req.params.id], function (err, result) {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.get("/delete/:id", (req, res) => {
+  const sql = "DELETE FROM users WHERE id = ?";
+  con.query(sql, [req.params.id], function (err, result) {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.post("/create", (req, res) => {
+  const sql = "INSERT INTO users SET ?";
+  con.query(sql, req.body, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("/");
+  });
+});
+
+app.get("/create", (req, res) => {
+  res.render("form");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
